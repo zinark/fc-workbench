e0f837421367eee32c3599dc0ce098134c1f954f
ferhat <ceferhat@gmail.com>
2023-04-23 15:59:43 +0300

    next env url

--- fc-workbench-next-ui/fc-workbench/service/WorkbenchService.js
+++ fc-workbench-next-ui/fc-workbench/service/WorkbenchService.js
@@ -1,5 +1,6 @@
 export const WorkbenchService = {
         listWorkbenchs() {
+            const baseUrl = process.env.BASE_URL;
             return fetch('/data/workbenchs.json', {headers: {'Cache-Control': 'no-cache'}})
                 .then((res) => res.json())
                 .then((d) => d);
--- fc-workbench-next-ui/next.config.js
+++ fc-workbench-next-ui/next.config.js
@@ -1,5 +1,9 @@
+const process = require("next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        BASE_URL: 'http://localhost:5000'
+    },
     reactStrictMode: false
 };
 
