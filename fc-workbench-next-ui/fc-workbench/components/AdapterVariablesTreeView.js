0389213026641a95de1aa123dc27190bba59096a
ferhat <ceferhat@gmail.com>
2023-05-04 22:27:19 +0300

    Update AdapterVariablesTreeView.js

--- fc-workbench-next-ui/fc-workbench/components/AdapterVariablesTreeView.js
+++ fc-workbench-next-ui/fc-workbench/components/AdapterVariablesTreeView.js
@@ -1,12 +1,17 @@
 import {InputText} from "primereact/inputtext";
 import {Tree} from "primereact/tree";
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import {Utils} from "../service/Utils";
 import Enumerable from 'linq'
+import {Button} from "primereact/button";
+import {OverlayPanel} from "primereact/overlaypanel";
+import {Toast} from "primereact/toast";
 
 const AdapterVariablesTreeView = (props) => {
     let adapter = props.adapter
     let parts = adapter.parts ?? []
+    const op = useRef(null);
+    const toast = useRef(null);
 
     const [keywordPart, setKeywordPart] = useState(null)
 
@@ -26,15 +31,44 @@ const AdapterVariablesTreeView = (props) => {
         // adapter.requests.map(req => {
         //     if (req.content.indexOf(variable.adapterKey) > 0) list.push(req);
         // })
-        return <div className="p-0 m-0"> {variable.adapterKey} <br/>
+        return <>
+            <div type="button" label={variable.adapterKey} onClick={e => op.current.toggle(e)} severity="success">
+                {variable.adapterKey}
+            </div>
+            <OverlayPanel ref={op}
+                          appendTo={typeof window !== 'undefined' ? document.body : null}
+                          showCloseIcon
+                          id="overlay_panel"
+                          style={{width: '450px'}}>
+                <div>
+                    <h4>{variable.type}</h4>
+                    <pre>{JSON.stringify(variable, null, 4)}</pre>
+                    <Button className="m-1" label="Save"></Button>
+                    <Button className="m-1" label="Close"></Button>
+                </div>
+                {/*<DataTable value={products} selection={selectedProduct} onSelectionChange={(e) => setSelectedProduct(e.value)} selectionMode="single" responsiveLayout="scroll" paginator rows={5} onRowSelect={onProductSelect}>*/}
+                {/*    <Column field="name" header="Name" sortable headerStyle={{ minWidth: '10rem' }} />*/}
+                {/*    <Column header="Image" body={imageBodyTemplate} headerStyle={{ minWidth: '10rem' }} />*/}
+                {/*    <Column field="price" header="Price" body={priceBodyTemplate} sortable headerStyle={{ minWidth: '8rem' }} />*/}
+                {/*</DataTable>*/}
+            </OverlayPanel>
 
-            {/*{list.map((x, ix) =>*/}
-            {/*    <Link key={ix}*/}
-            {/*          href={`/workbench/${1}/adapter/${adapter.refNo}/request/${x.refNo}`}>*/}
-            {/*        {x.code}*/}
-            {/*    </Link>*/}
-            {/*)}*/}
-        </div>
+            {/*<SplitButton*/}
+            {/*    className="p-1 m-1"*/}
+            {/*    icon="pi pi-check"*/}
+            {/*    tooltip="tool tip"*/}
+            {/*    label={variable.adapterKey}></SplitButton>*/}
+        </>
+
+        // <div className="p-0 m-0"> {variable.adapterKey} <Button>test</Button>
+        //
+        //     {/*{list.map((x, ix) =>*/}
+        //     {/*    <Link key={ix}*/}
+        //     {/*          href={`/workbench/${1}/adapter/${adapter.refNo}/request/${x.refNo}`}>*/}
+        //     {/*        {x.code}*/}
+        //     {/*    </Link>*/}
+        //     {/*)}*/}
+        // </div>
     }
 
     const nodes = Enumerable.from(parts).select(part => ({
@@ -51,12 +85,16 @@ const AdapterVariablesTreeView = (props) => {
             'pi-dollar')
     })).toArray()
 
-    let onDoubleClick = (x) =>{}
-    let onClick = (x) =>{}
+    let onDoubleClick = (x) => {
+        toast.current.show({severity: 'info', summary: 'Event!', detail: 'Double clicked', life: 3000});
+    }
+    let onClick = (x) => {
+    }
     if (props.onDoubleClick) onDoubleClick = props.onDoubleClick;
-    if (props.onClick) onClick= props.onClick;
+    if (props.onClick) onClick = props.onClick;
 
     return <>
+        <Toast ref={toast}/>
         <h4>Parts</h4>
         <InputText value={keywordPart}
                    placeholder="part search"
